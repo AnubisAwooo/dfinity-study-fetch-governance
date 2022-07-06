0829b3774cf598ef425ab7d679f29d30b37c90e3
anubis <anubis@icpleague.com>
2022-07-06 17:57:45 +0800

    study

--- canisters/fetcher/src/main.rs
+++ canisters/fetcher/src/main.rs
@@ -117,3 +117,12 @@ fn http_request() {
         },));
     });
 }
+
+#[cfg(any(target_arch = "wasm32", test))]
+fn main() {}
+
+#[cfg(not(any(target_arch = "wasm32", test)))]
+fn main() {
+    ic_cdk::export::candid::export_service!();
+    std::println!("{}", __export_service());
+}
